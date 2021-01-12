481ecd4589ec905a8988cfb4c9184988cb4fdc56
SehoonKwon <49121446+SehoonKwon@users.noreply.github.com>
2021-01-12 21:19:00 +0900

    Add files via upload

--- TCP_IP_Programming_serv/TCP_IP_Programming_serv/hello_server_win.cpp
+++ TCP_IP_Programming_serv/TCP_IP_Programming_serv/hello_server_win.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <cstring>
 #include <WinSock2.h>
+#include <string>
 using namespace std;
 
 #define endl "\n"
@@ -14,12 +15,6 @@ int main(int argc, char* argv[]) //argc는 옵션의 개수, argv는 옵션 배열
 
 	int szClntAddr;
 	char message[] = "Hello wolrd";
-	
-	if (argc != 2)
-	{
-		cout << "Usage : " << argv[0] << " <port>" << endl;
-		exit(1);
-	}
 
 	//winsock 초기화, 프로그램에서 요구하는 윈도우 소켓 버전을 알리고 해당 버전을 지원하는 라이브러리 초기화 작업
 	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) //MAKEWOR(주버전, 부버전), WSADATA 변수의 주소 값
@@ -29,10 +24,17 @@ int main(int argc, char* argv[]) //argc는 옵션의 개수, argv는 옵션 배열
 	if (hServSock == INVALID_SOCKET) // -1 상수값이지만 상수로 쓰는게 아니라 이렇게 사용자정의 변수로 사용하면 가독성도 좋고 후에 변화가 있어도 대처가능
 		cout << "socket Error" << endl;
 
+	string sPORT;
+	cout << "PORT 번호 입력 : ";
+	cin >> sPORT;
+
+	//아래 함수를 이용하기 위해 C 타입으로 형변환
+	const char* PORT = sPORT.c_str();
+
 	memset(&servAddr, 0, sizeof(servAddr)); //소켓의 sin_zero 부분을 0으로 채워야되는데 처음부터 0으로 채우고 필요한 앞부분에 새로 값 할당
 	servAddr.sin_family = AF_INET; //주소 체계
 	servAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY); //여긴 책이랑 형태가 조금 다름. S_un 이후에 addr이 나옴, INADDR_ANY = 컴퓨터의 IP주소 자동할당
-	servAddr.sin_port = htons(atoi(argv[1])); //htons = host byte to network byte short(타입형태)
+	servAddr.sin_port = htons(atoi(PORT)); //htons = host byte to network byte short(타입형태)
 
 	if (bind(hServSock, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR) //2번째인자를 SOCKADDR 형변환을 이용해 IP와 Port번호 할당 한번에 해주기
 		cout << "bind Error" << endl;
